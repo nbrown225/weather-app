49f8111c9c259bcda41d2a337b9460eb99a85e06
nbrown225 <chbrown225@yahoo.com>
2022-08-02 19:53:35 -0700

    saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,29 @@
-var day1 = document.querySelector('#day1')
-day1.addEventListener('click', function() {
-    window.alert('hi')
-})
+
+
+// API KEY
+const API_KEY = "43a7fd61b9f2df3eab9c7cf15eae3f6c";
+// DOM QUERY
+var searchBtn = document.querySelector("#search-btn");
+var searchHistory = document.querySelector('#history');
+var currentCity = document.querySelector('#current-city');
+var day1 = document.querySelector('#day1');
+var day2 = document.querySelector('#day2');
+var day3 = document.querySelector('#day3');
+var day4 = document.querySelector('#day4');
+var day5 = document.querySelector('#day5');
+
+// get city
+var city = "";
+function getCity() {
+    fetch("https://api.openweatermap.org/data/2.5/weather?q=" + city + "&appid" + API_KEY)
+    .then(function(resp) { return resp.json() })
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function(){
+    }
+    )
+}
+window.onload = function() {
+    getCity( )
+}
